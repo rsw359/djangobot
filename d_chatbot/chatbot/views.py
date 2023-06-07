adde833f1ee82cde4093eab0014dc101f1a630ae
Roger Wells <rsw359@gmail.com>
2023-06-07 11:07:16 +0900

    templates and scripts added

--- d_chatbot/chatbot/views.py
+++ d_chatbot/chatbot/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+
+
+def chatbot(request):
+    if request.method == 'POST':
+        message = request.POST.get('message')
+        response = 'This is a response from Minortron'
+        return JsonResponse({"message": message, 'response': response})
+    return render(request, 'chatbot.html')
