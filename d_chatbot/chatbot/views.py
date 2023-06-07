e94d30cc7a58199cce05117694a1188b4e8aed38
Roger Wells <rsw359@gmail.com>
2023-06-07 14:22:12 +0900

    chatbot complete

--- d_chatbot/chatbot/views.py
+++ d_chatbot/chatbot/views.py
@@ -1,12 +1,30 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from .config import API_KEY
+import openai
 
-# Create your views here.
+
+openai.api_key = API_KEY
+
+
+def ask_gpt3(message):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=message,
+        max_tokens=150,
+        n=1,
+        stop=None,
+        temperature=0.7,
+    )
+    print(response)
+    # strips formatting from the response
+    answer = response.choices[0].text.strip()
+    return answer
 
 
 def chatbot(request):
     if request.method == 'POST':
         message = request.POST.get('message')
-        response = 'This is a response from Minortron'
+        response = ask_gpt3(message)
         return JsonResponse({"message": message, 'response': response})
     return render(request, 'chatbot.html')
