59ad4600595787f94cba816769c9fe6ce4aee654
Roger Wells <rsw359@gmail.com>
2023-06-13 17:37:39 +0900

    fixed error with the created_at parameter in the user model

--- d_chatbot/chatbot/models.py
+++ d_chatbot/chatbot/models.py
@@ -8,7 +8,7 @@ class Chat(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     message = models.TextField()
     response = models.TextField()
-    created = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'{self.user.username}:{self.message}'
